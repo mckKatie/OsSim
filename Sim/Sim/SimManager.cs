2460f09e3cecfb25874d73dffda4b9f96ec67603
jburge <jburge16@my.whitworth.edu>
2016-04-01 16:36:36 -0700

    new file:   Sim.sln 	new file:   Sim.v12.suo 	new file:   Sim/App.config 	new file:   Sim/Metadata.cs 	new file:   Sim/ProcessControlBLock.cs 	new file:   Sim/Program.cs 	new file:   Sim/Properties/AssemblyInfo.cs 	new file:   Sim/Sim.csproj 	new file:   Sim/SimManager.cs 	new file:   Sim/Temp.cs 	new file:   Sim/bin/Debug/Sim.exe.config 	new file:   Sim/bin/Debug/Sim.vshost.exe 	new file:   Sim/bin/Debug/Sim.vshost.exe.config 	new file:   Sim/bin/Debug/Sim.vshost.exe.manifest 	new file:   Sim/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   Sim/obj/Debug/Sim.csproj.FileListAbsolute.txt 	new file:   Sim/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   Sim/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   Sim/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs

---
+++ Sim/Sim/SimManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sim
+{
+    class SimManager
+    {
+        int clock;
+        Dictionary<int, ProcessControlBLock> processes;
+        List<Tuple<int, int>> subTimes;
+
+
+        List<Tuple<int, int>> IOList; //dont know what to call this <outTime, PID>
+
+        public void CheckIOStatus(int currentTime) //check procList for processes ready to be placed in wait queue
+        {
+            while(true)
+            {
+                if(IOList[0].Item1 == currentTime)
+                {
+
+                }
+            }
+        }
+
+        public void StartIO(int PID)
+        {
+            // get PID and compute current time + IO burst duration
+            // make this a tuple
+            // place in list and sort on outtime
+        }
+    }
+ 
+}
