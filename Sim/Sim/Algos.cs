66053ec8f2a92a21023c38cb9a62ef6241f02a2b
lmcgill8 <lumcgill8@gmail.com>
2016-04-01 18:55:51 -0700

    modified:   Sim/.vs/Sim/v14/.suo 	new file:   Sim/Sim/Algos.cs 	modified:   Sim/Sim/Sim.csproj 	modified:   Sim/Sim/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	modified:   Sim/Sim/obj/Debug/Sim.csproj.FileListAbsolute.txt

---
+++ Sim/Sim/Algos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sim
+{
+    class RoundRobin
+    {
+        int quantum = 20;
+
+        int getQuantum()
+        {
+            return quantum;
+        }
+
+        void setQuantum(int x)
+        {
+            quantum = x;
+        }
+
+        void run()
+        {
+            
+        }
+    }
+}
