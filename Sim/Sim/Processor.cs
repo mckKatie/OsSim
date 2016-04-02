901edeb1d08c1b14bbbe43e09614717fa58ffa76
jburge <jburge16@my.whitworth.edu>
2016-04-01 17:10:14 -0700

    modified:   Sim/Processor.cs

--- Sim/Sim/Processor.cs
+++ Sim/Sim/Processor.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+enum Pstate { busy, open, stop}
+
 namespace Sim
 {
     class Processor
     {
+        public Pstate state;
+        int PID;
+        int burstCompletionTime; // either because burst is completed or quantum reached
+
+        Processor(){
+            state = Pstate.open;
+        }
+        public void CheckStatus(int currentTime)
+        {
+            if (burstCompletionTime == currentTime)
+            {
+                state = Pstate.stop;
+            }
+        }
+
+        public void ContextSwap(int _PID, int _burstCompletionTime) // burst completion time needs to be set to sooner of burst time and quantum in os strategy
+        {
+            PID = _PID;
+            burstCompletionTime = _PID;
+            state = Pstate.busy;
+        }
+
+        public int FreeProcessor()
+        {
+            state = Pstate.open;
+            return PID;
+        }
     }
 }
